55ee90816d304e9201df5c3511fb23298ca9af80
Maxence Nesme <maxencenesme@gmail.com>
2017-05-22 14:29:19 +0200

    m

--- rails-stupid-coaching/config/routes.rb
+++ rails-stupid-coaching/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'answer', to: 'coachingcontroller#answer'
+
+  get 'ask', to: 'coachingcontroller#ask'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
